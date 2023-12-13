e3e40768e6fa425d810e388c0ef4e35a0c2ac90a
FrancescoVenanti <francesco.venanti@gmail.com>
2023-12-13 17:32:38 +0100

    jikwsedfvbjkwrefvhbkewf

--- assets/scripts/artist.js
+++ assets/scripts/artist.js
@@ -4,32 +4,32 @@ const artistId = params.get("artistId");
 console.log(artistId);
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetch(urlArtist + artistId, {
-        headers: {
-            "X-RapidAPI-Key": token,
-            "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-        },
-    })
-        .then((resp) => {
-            if (!resp.ok) {
-                statusErrors(resp);
-            }
-            return resp.json();
-        })
-        .then((myObj) => {
-            console.log(myObj);
-            generateArtistPage(myObj);
-        })
-        .catch((err) => {
-            console.log("error", err);
-        });
+	fetch(urlArtist + artistId, {
+		headers: {
+			"X-RapidAPI-Key": token,
+			"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+		},
+	})
+		.then((resp) => {
+			if (!resp.ok) {
+				statusErrors(resp);
+			}
+			return resp.json();
+		})
+		.then((myObj) => {
+			console.log(myObj);
+			generateArtistPage(myObj);
+		})
+		.catch((err) => {
+			console.log("error", err);
+		});
 });
 
 function generateArtistPage(myObj) {
-    const imgArtistDiv = document.getElementById("imgArtist");
-    imgArtistDiv.style.backgroundImage = myObj.picture_xl;
-    const artistName = document.getElementById("artistName");
-    artistName.innerText = myObj.name;
-    const monthlyFans = document.getElementById("monthlyFans");
-    monthlyFans.innerText = myObj.nb_fan + " ascoltatori mensili";
+	const imgArtistDiv = document.getElementById("imgArtist");
+	imgArtistDiv.style.backgroundImage = `url('${myObj.picture_xl}')`;
+	const artistName = document.getElementById("artistName");
+	artistName.innerText = myObj.name;
+	const monthlyFans = document.getElementById("monthlyFans");
+	monthlyFans.innerText = myObj.nb_fan + " ascoltatori mensili";
 }
