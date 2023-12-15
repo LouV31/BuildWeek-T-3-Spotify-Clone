e0b85bf665036ce32723daa5f45ff360e50ab738
FrancescoVenanti <francesco.venanti@gmail.com>
2023-12-15 12:18:21 +0100

    adjustments

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,13 +1,13 @@
 const urlArtist = "https://deezerdevs-deezer.p.rapidapi.com/artist/";
 const urlAlbum = "https://deezerdevs-deezer.p.rapidapi.com/album/";
 const urlPlaylist = "https://deezerdevs-deezer.p.rapidapi.com/playlist/";
+const ulrTrack = "https://deezerdevs-deezer.p.rapidapi.com/track/";
 
 const artistArr = [13, 45, 29, 19, 39, 84];
 const albumsArr = [103248, 12813058, 14101380, 531851, 74479];
 const playlistArr = [
-<!--playlistFatta-->
-    160504851, 49119317, 6313525084, 31693005, 5317014122, 12146394, 4525618, 3005155, 114461181, 133196991,
-
+	/* <!--playlistFatta--> */
+	160504851, 49119317, 6313525084, 31693005, 5317014122, 12146394, 4525618, 3005155, 114461181, 133196991,
 ];
 
 const idAdsArr = [89748835, 565278172, 23457141];
@@ -160,23 +160,23 @@ const fetchPlaylists = () => {
 };
 fetchPlaylists();
 function generatePlaylist(obj) {
-<!--playlistFatta-->
-    const albumsRow = document.getElementById("playlistRow");
-    const colAlbum = document.createElement("div");
-    colAlbum.className = "col";
-    const cardContainer = document.createElement("div");
-    cardContainer.className = "card bg-dark p-3 rounded-1";
-    const containerImage = document.createElement("div");
-    containerImage.className = "position-relative";
-    const albumImg = document.createElement("img");
-    albumImg.src = obj.picture_small;
-    albumImg.className = "card-img-top rounded-1 mb-3";
-    const buttonDiv = document.createElement("div");
-    buttonDiv.className = "play-button";
-    const playButton = document.createElement("button");
-    playButton.className = "d-flex top-100 start-100 me-2 mb-4 p-3 border-0 text-black bg-success rounded-circle";
-    playButton.style.cursor = "pointer";
-    playButton.innerHTML = `<svg
+	/* <!--playlistFatta--> */
+	const albumsRow = document.getElementById("playlistRow");
+	const colAlbum = document.createElement("div");
+	colAlbum.className = "col";
+	const cardContainer = document.createElement("div");
+	cardContainer.className = "card bg-dark p-3 rounded-1";
+	const containerImage = document.createElement("div");
+	containerImage.className = "position-relative";
+	const albumImg = document.createElement("img");
+	albumImg.src = obj.picture_small;
+	albumImg.className = "card-img-top rounded-1 mb-3";
+	const buttonDiv = document.createElement("div");
+	buttonDiv.className = "play-button";
+	const playButton = document.createElement("button");
+	playButton.className = "d-flex top-100 start-100 me-2 mb-4 p-3 border-0 text-black bg-success rounded-circle";
+	playButton.style.cursor = "pointer";
+	playButton.innerHTML = `<svg
 
         xmlns="http://www.w3.org/2000/svg"
         width="12"
@@ -187,16 +187,15 @@ function generatePlaylist(obj) {
         >
         <polygon points="0 0 16 8 0 16" />
         </svg>`;
-<!--playlistFatta-->
-    const albumBody = document.createElement("div");
-    albumBody.className = "card-body p-0";
-    const albumTitle = document.createElement("h5");
-    albumTitle.className = "card-title text-white";
-    albumTitle.innerText = obj.title;
-    const albumDesc = document.createElement("p");
-    albumDesc.className = "card-text text-white-50";
-    albumDesc.innerText = obj.creator.name;
-
+	/* <!--playlistFatta--> */
+	const albumBody = document.createElement("div");
+	albumBody.className = "card-body p-0";
+	const albumTitle = document.createElement("h5");
+	albumTitle.className = "card-title text-white";
+	albumTitle.innerText = obj.title;
+	const albumDesc = document.createElement("p");
+	albumDesc.className = "card-text text-white-50";
+	albumDesc.innerText = obj.creator.name;
 
 	containerImage.appendChild(albumImg);
 	buttonDiv.appendChild(playButton);
@@ -208,22 +207,21 @@ function generatePlaylist(obj) {
 	colAlbum.appendChild(cardContainer);
 	albumsRow.appendChild(colAlbum);
 
-<!--playlistFatta-->
-    colAlbum.addEventListener("mouseover", function () {
-        buttonDiv.classList.add("opacity-100");
-    });
-    colAlbum.addEventListener("mouseout", function () {
-        buttonDiv.classList.remove("opacity-100");
-    });
-    cardContainer.addEventListener("click", function () {
-        window.location.assign("./playlist.html?playlistId=" + obj.id);
-    });
-
+	/* <!--playlistFatta--> */
+	colAlbum.addEventListener("mouseover", function () {
+		buttonDiv.classList.add("opacity-100");
+	});
+	colAlbum.addEventListener("mouseout", function () {
+		buttonDiv.classList.remove("opacity-100");
+	});
+	cardContainer.addEventListener("click", function () {
+		window.location.assign("./playlist.html?playlistId=" + obj.id);
+	});
 }
 
 const fetchAds = function () {
 	shuffleArray(idAdsArr);
-	fetch(urlPlaylist + idAdsArr[0], {
+	fetch(ulrTrack + idAdsArr[0], {
 		headers: {
 			"X-RapidAPI-Key": token,
 			"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
@@ -240,7 +238,7 @@ const fetchAds = function () {
 			generateAds(myObj);
 		});
 };
-//fetchAds();
+fetchAds();
 
 function generateAds(obj) {
 	const imgAds = document.getElementById("imgAds");
